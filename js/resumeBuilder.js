ef7e9fa851b0ca0d20acaba63a7a90dbee917afa
Justin Huynh <j.alexhuynh@gmail.com>
2017-01-20 15:32:31 -0800

    Add main header name and role.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// $("#main").append("Justin Huynh");
+
+// var awesomeThoughts = "I am Justin Huynh and I am AWESOME!";
+// console.log(awesomeThoughts);
+
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// $("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%", "Justin Huynh");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
