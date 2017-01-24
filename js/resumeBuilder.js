f96624e735245e652fdf7432e01a508ae654153b
Justin Huynh <j.alexhuynh@gmail.com>
2017-01-23 18:34:19 -0800

    Add header & contact info, and picture.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,13 +1,30 @@
-// $("#main").append("Justin Huynh");
+var bio = {
+	"name" : "Justin Huynh",
+	"role" : "Web Developer",
+	"contacts" : {
+		"mobile" : "951-310-8018",
+		"email" : "J.alexhuyh@gmail.com",
+		"github" : "Jalexhuynh",
+		"location" : "Corona, CA"
+	},
+	"picture" : "images/me.jpg",
+	"welcome" : "Hi, welcome to my blog!",
+	"skills" : ["Programming, Web Design, Microbiology", "Graphic Design"]
+}
 
-// var awesomeThoughts = "I am Justin Huynh and I am AWESOME!";
-// console.log(awesomeThoughts);
+var prependToHeader = function(item, how) {
+	$("#header").prepend(how.replace("%data%", item));
+};
 
-// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-// $("#main").append(funThoughts);
+var appendToResume = function(item, how, where) {
+	$(where).append(how.replace("%data%", item));
+};
 
-var formattedName = HTMLheaderName.replace("%data%", "Justin Huynh");
-var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+prependToHeader(bio.role, HTMLheaderRole);
+prependToHeader(bio.name, HTMLheaderName);
+appendToResume(bio.contacts.mobile, HTMLmobile, "#topContacts");
+appendToResume(bio.contacts.email, HTMLemail, "#topContacts");
+appendToResume(bio.contacts.github, HTMLgithub, "#topContacts");
+appendToResume(bio.contacts.location, HTMLlocation, "#topContacts");
+appendToResume(bio.picture, HTMLbioPic, "#header");
+appendToResume(bio.welcome, HTMLwelcomeMsg, "#header");
